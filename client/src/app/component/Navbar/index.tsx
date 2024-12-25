2107f87fe68414842f1fecb3261897488415d2f5
Suraj <skr.suraj26@gmail.com>
2024-12-25 21:28:57 +0530

    third commit

---
+++ client/src/app/component/Navbar/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between bg-white px-4 py-4 dark:bg-black dark:px-4 dark:py-3">
+      
+      <div className="flex items-center gap-8">
+        <div className="relative flex h-min w-[200px}">
+          
+
+        </div>
+
+      </div>
+      </div>
+  )
+}
+
+export default Navbar
