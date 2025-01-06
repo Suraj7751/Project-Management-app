064c3a4bf58f4ff80a5d8bf0aa23700be9a2e190
Suraj <skr.suraj26@gmail.com>
2025-01-06 23:33:27 +0530

    4th commit

--- client/src/app/component/Navbar/index.tsx
+++ client/src/app/component/Navbar/index.tsx
@@ -1,20 +1,36 @@
-import React from 'react'
-
+import React from "react";
+import { Search, Settings } from "lucide-react";
+import dynamic from 'next/dynamic';
 
 
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between bg-white px-4 py-4 dark:bg-black dark:px-4 dark:py-3">
-      
       <div className="flex items-center gap-8">
         <div className="relative flex h-min w-[200px}">
-          
-
+          <Search className="absolute left-[4ox] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white" />
+          <input
+            className="w-full rounded border-none bg-gray-100 p-2 pl-8 placeholder-gray-500 focus:border-transparent focus:outline-none dark:bg-gray-700 dark:text-white dark:placeholder-white"
+            type="search"
+            placeholder="Search ..."
+          />
         </div>
+      </div>
+
+      <div className="flex items-center">
+        <a
+        href="/settings"
+        className="h-min w-min rounded p-2 hover:bg-gray-100"
+        >
+          <Settings className="h-6 w-6 cursor-pointer dark:text-white" />
+
+          
+        </a>
+        <div className="ml-2 mr-5 hidden min-h-[2em] w-[0.1rem] bg-gray-200 md:inline-block"></div>
 
       </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Navbar from './component/Navbar'
+import Navbar from "@/app/component/Navbar";
+import Sidebar from "@/app/component/Sidebar";
 
 type Props = {}
 
@@ -7,7 +8,7 @@ const dashboardWrapper = ({children}: {children: React.ReactNode}) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
 
-        sidebar
+        <Sidebar />
         <main
          className= {'dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64'}>
             
